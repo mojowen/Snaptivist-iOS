c270e174f81c77ce183ac6838c11e7cef203854c
Scott Duncombe <srduncombe@gmail.com>
2013-06-08 18:09:12 -0700

    adding model for controlling the tabs

---
+++ Snaptivist/SnaptivistTabs.m
@@ -0,0 +1,13 @@
+//
+//  SnaptivistTabs.m
+//  Snaptivist
+//
+//  Created by Scott Duncombe on 6/8/13.
+//  Copyright (c) 2013 Scott Duncombe. All rights reserved.
+//
+
+#import "SnaptivistTabs.h"
+
+@implementation SnaptivistTabs
+
+@end
