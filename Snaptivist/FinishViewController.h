b85e52907c689d5810b210948671ddaa2c1ef7cf
Scott Duncombe <srduncombe@gmail.com>
2013-06-27 01:32:11 -0700

    finished controller

---
+++ Snaptivist/FinishViewController.h
@@ -0,0 +1,14 @@
+//
+//  FinishViewController.h
+//  Snaptivist
+//
+//  Created by Scott Duncombe on 6/27/13.
+//  Copyright (c) 2013 Scott Duncombe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SnaptivistTabs.h"
+
+@interface FinishViewController : UIViewController
+
+@end
