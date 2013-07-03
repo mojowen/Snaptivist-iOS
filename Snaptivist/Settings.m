0ef0db7965b7ad06641765b35a6a7c085f32a236
Scott Duncombe <srduncombe@gmail.com>
2013-07-03 10:11:35 -0700

    use heroku

--- Snaptivist/Settings.m
+++ Snaptivist/Settings.m
@@ -41,8 +41,8 @@
 - (void)viewDidLoad
 {
     context = [[self appDelegate] managedObjectContext];
-//    NSString *base_URL = @"http://snaptivist.herokuapp.com";
-    NSString *base_URL = @"http://192.168.2.5:5050";
+    NSString *base_URL = @"http://snaptivist.herokuapp.com";
+//    NSString *base_URL = @"http://192.168.2.5:5050";
 
     self.objectManager = [RKObjectManager managerWithBaseURL:[NSURL URLWithString:base_URL]];
 
