94164e6dc3d138d56ccfcd4ff21da4253d6f9126
Scott Duncombe <srduncombe@gmail.com>
2013-07-05 18:59:55 -0700

    hidding memory logging stuff

--- Snaptivist/SnaptivistTabs.m
+++ Snaptivist/SnaptivistTabs.m
@@ -51,9 +51,6 @@
 -(void)startOver{
     [activeButton setBackgroundImage:inactiveImage forState:UIControlStateNormal];
 
-    NSLog(@"begin start over");
-    [self logMemUsage];
-
     signup = nil;
     
     [photosViewController removeFromParentViewController];
@@ -65,14 +62,9 @@
     repsViewController = nil;
     photosViewController = nil;
 
-    NSLog(@"after removing view controllers");
-    [self logMemUsage];
-
     [self getStarted];
     [self goToPhoto];
     
-    NSLog(@" after getstarted");
-    [self logMemUsage];
 
 }
 
@@ -157,15 +149,11 @@
     return (AppDelegate *)[[UIApplication sharedApplication] delegate];
 }
 - (void)setChildFrame:(UIViewController *)viewController {
-    [self logMemUsage];
 
     if( [self.view.subviews count] > 0 ) {
         [[self.view.subviews objectAtIndex:0] removeFromSuperview];
     }
-    NSLog(@" After remove from superview");
     
-    [self logMemUsage];
-
     [self.view addSubview:viewController.view];
     [self.view sendSubviewToBack:viewController.view];
     viewController.view.frame = self.view.frame;
@@ -177,33 +165,4 @@
     activeButton = button;
 }
 
-
-vm_size_t usedMemory(void) {
-    struct task_basic_info info;
-    mach_msg_type_number_t size = sizeof(info);
-    kern_return_t kerr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&info, &size);
-    return (kerr == KERN_SUCCESS) ? info.resident_size : 0; // size in bytes
-}
-
-vm_size_t freeMemory(void) {
-    mach_port_t host_port = mach_host_self();
-    mach_msg_type_number_t host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
-    vm_size_t pagesize;
-    vm_statistics_data_t vm_stat;
-    
-    host_page_size(host_port, &pagesize);
-    (void) host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size);
-    return vm_stat.free_count * pagesize;
-}
--(void)logMemUsage {
-    // compute memory usage and log if different by >= 100k
-    static long prevMemUsage = 0;
-    long curMemUsage = usedMemory();
-    long memUsageDiff = curMemUsage - prevMemUsage;
-    
-//    if (memUsageDiff > 100000 || memUsageDiff < -100000) {
-        prevMemUsage = curMemUsage;
-        NSLog(@"Memory used %7.1f (%+5.0f), free %7.1f kb", curMemUsage/1000.0f, memUsageDiff/1000.0f, freeMemory()/1000.0f);
-//    }
-}
 @end
