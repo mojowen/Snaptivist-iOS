10272be840038935bfdd6e4e50c84200fb95f90f
Scott Duncombe <srduncombe@gmail.com>
2013-07-08 18:01:32 -0700

    adding placeholder constants

---
+++ Snaptivist/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  
+//
+//  Created by Scott Duncombe on 7/8/13.
+//
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
---
+++ Snaptivist/Constants_off.h
@@ -0,0 +1,34 @@
+//
+//  Constants_off.h
+//  Snaptivist
+//
+//  Created by Scott Duncombe on 7/8/13.
+//  Copyright (c) 2013 Scott Duncombe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+// This file is not included in the git repository and is a good place for storing files
+// Change the file name to Constants.h to fix
+
+#define ACCESS_KEY_ID          @"YOUR ID"
+#define SECRET_KEY             @"YOUR SECRET"
+
+#define PICTURE_BUCKET         @"YOUR BUCKET"
+
+#define CREDENTIALS_ERROR_TITLE    @"Missing Credentials"
+#define CREDENTIALS_ERROR_MESSAGE  @"AWS Credentials not configured correctly.  Please review the README file."
+
+#ifdef DEBUG
+#define BASE_URL    @"http://localhost.com:5050"
+#else
+#define BASE_URL    @"http://snaptivist.herokuapp.com"
+#endif
+
+
+
+// Change this file
+@interface Constants : NSObject
+
+@end
